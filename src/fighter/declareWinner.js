76cb1a64463abc189643442f54cab7122d27841f
BeydullaZeynalov <zeynalov.beyduall97@gmail.com>
2021-03-23 11:17:30 +0100

    green

--- src/fighter/declareWinner.js
+++ src/fighter/declareWinner.js
@@ -6,7 +6,13 @@ const  subtractDamage = (fighter1, fighter2) => {
     fighter1.health = fighter1.health - fighter2.damage;
 }
 
+const lost = (fighter) => {
+    if(fighter.health === 0){
+        return true;
+    }
+}
 module.exports = {
     declareWinner,
-    subtractDamage
+    subtractDamage,
+    lost
 }
