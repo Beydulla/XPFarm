a43253a5b78d53c335b911bf13d1492abc8f9389
BeydullaZeynalov <zeynalov.beyduall97@gmail.com>
2021-03-23 10:41:54 +0100

    test for assigning fighter  name

---
+++ src/fighter/fighter.test.js
@@ -0,0 +1,11 @@
+const { describe, test, expect } = require('@jest/globals');
+const { Fighter } = require("./Fighter")
+
+describe('Tests for fighter kata', () => {
+    describe('Tests for validation of fighter', () => {
+        test("Expect fighter to have name", () => {
+            const fighter = new Fighter("Lew", 2, 3);
+            expect(fighter.name).toEqual('Lew');
+        });
+    });
+});
