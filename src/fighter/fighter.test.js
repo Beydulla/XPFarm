8474586b47ae5bf4602b2b948ae5ac25746ec982
BeydullaZeynalov <zeynalov.beyduall97@gmail.com>
2021-03-23 10:45:55 +0100

    test for assignin health

--- src/fighter/fighter.test.js
+++ src/fighter/fighter.test.js
@@ -7,5 +7,9 @@ describe('Tests for fighter kata', () => {
             const fighter = new Fighter("Lew", 2, 3);
             expect(fighter.name).toEqual('Lew');
         });
+        test("Expect fighter to have health", () => {
+            const fighter = new Fighter("Lew", 2, 3);
+            expect(fighter.health).toEqual(2);
+        });
     });
 });
